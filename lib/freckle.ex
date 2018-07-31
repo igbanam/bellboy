b551cf532075ec6f11334f5a6237eebbfb54525a
Owajigbanam Ogbuluijah <igbanam@andela.com>
2018-07-31 17:47:55 +0100

    Initial commit

---
+++ lib/freckle.ex
@@ -0,0 +1,18 @@
+defmodule Freckle do
+  @moduledoc """
+  Documentation for Freckle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Freckle.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/freckle_test.exs
@@ -0,0 +1,8 @@
+defmodule FreckleTest do
+  use ExUnit.Case
+  doctest Freckle
+
+  test "greets the world" do
+    assert Freckle.hello() == :world
+  end
+end
