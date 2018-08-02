c0b6625efaced2a3602317143317f9bef43cbf4a
Owajigbanam Ogbuluijah <igbanam@andela.com>
2018-08-02 16:23:06 +0100

    Create Help
    Every good command line tool has a robust "Help" section. This instructs
    the user on how to use the tool, what to look out for, gotchas, and
    could have some shameful marketing to go along with it.
    
    I want to add a help section to the tool. This help is going to be
    minimal for now because my purview for the application at this point is
    quite limited. The text in this help would be revised moving forward
    
    This is done by:
    - Showing...
        - The syntax for running the application
        - Possible flags one could use to run the application and what these
          flags mean
        - Shameful marketing pointing people to this GitHub account
    
    Resolves #2

--- lib/freckle.ex
+++ lib/freckle.ex
@@ -4,15 +4,46 @@ defmodule Freckle do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Freckle.hello
-      :world
-
+  Enrtry Point
   """
-  def hello do
-    :world
+  def main(args \\ []) do
+     args
+      |> digest_arguments
+      |> process
+  end
+
+  defp digest_arguments(args) do
+    options =
+      args
+      |> OptionParser.parse(
+        switches: [help: :boolean, version: :boolean],
+        aliases: [h: :help, v: :version])
+
+    case options do
+      { [ help: true ], _, _ }    -> :help
+      { [ version: true ], _, _ } -> :version
+      _                           -> :help
+    end
+  end
+
+  defp process(:help) do
+    """
+    USAGE
+    =====
+      freckle --project PROJECT --for HH:MM:SS
+      freckle [-h | --help]
+      freckle [-v | --version]
+
+    ARGS
+    ====
+
+        PROJECT    The project you want to log time for
+        HH:MM:SS   Hours, minutes, and seconds you want to log
+    """
+    |> IO.puts
+  end
+
+  defp process(:version) do
+    IO.puts("Version 0.0.0")
   end
 end
